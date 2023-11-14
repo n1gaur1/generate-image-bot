9048616d37d2b315b76d8ca54ed1a68262ad2097
m0rinphen <m0rinphen.zzz@gmail.com>
2023-11-14 10:07:44 +0900

    add imgur token

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,6 +3,8 @@ export const getEnv = () => {
     DISCORD_BOT_TOKEN,
     DISCORD_APP_ID,
     DISCORD_GUILD_ID,
+    IMGUR_CLIENT_ID,
+    IMGUR_CLIENT_SECRET,
   } = process.env;
 
   if (!DISCORD_BOT_TOKEN) {
@@ -14,10 +16,18 @@ export const getEnv = () => {
   if (!DISCORD_GUILD_ID) {
     throw Error('Please set the environment variable DISCORD_GUILD_ID.');
   }
+  if (!IMGUR_CLIENT_ID) {
+    throw Error('Please set the environment variable IMGUR_CLIENT_ID.');
+  }
+  if (!IMGUR_CLIENT_SECRET) {
+    throw Error('Please set the environment variable IMGUR_CLIENT_SECRET.');
+  }
 
   return {
     discordBotToken: DISCORD_BOT_TOKEN,
     discordAppID: DISCORD_APP_ID,
     discordGuildID: DISCORD_GUILD_ID,
+    imgurClientID: IMGUR_CLIENT_ID,
+    imgurClientSecret: IMGUR_CLIENT_SECRET,
   };
 };
